da315ac0040777963040df7e021c418a88c03991
Shrutesh Patil <42747197+pasha009@users.noreply.github.com>
2021-05-25 00:04:21 +0530

    added scrapers for `timeanddate.com`, `panchange.astrosage.com`
    * added scraper for panchang.astrosage.com
    * added scraper for timeanddate.com
    * refactor and add basic readme for holidays folder
    * remove useless files left by mistake
    * holidays readme fix typo, table format

---
+++ holidays/README.md
@@ -0,0 +1,8 @@
+# Holidays
+
+scrapes websites to generate holiday data in csv format
+
+| script | website scraped |
+| --- | --- |
+| `panchang.py` |https://panchang.astrosage.com/calendars/indiancalendar|
+| `timeanddate.py` |https://www.timeanddate.com/holidays/india/|
--- national_holidays.py
+++ national_holidays.py
@@ -1,4 +1,4 @@
-# !usr/bin/env python3
+#!/usr/bin/env python3
 import os
 import pandas as pd
 from bs4 import BeautifulSoup
@@ -8,42 +8,46 @@ import argparse
 
 def args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-y', '--year',
-                        help='Year for which data is to be generated', type=int)
     parser.add_argument(
-        '-p', '--path', help='Path to store the file', type=str)
+        "-y", "--year", help="Year for which data is to be generated", type=int
+    )
+    parser.add_argument("-p", "--path", help="Path to store the file", type=str)
     args = parser.parse_args()
     return args
 
 
 def monthly_holidays(year, month):
-    url = f'http://www.india.gov.in/calendar?date={year}-{month}'
+    url = f"http://www.india.gov.in/calendar?date={year}-{month}"
     print(f"Currently Parsing:{url}")
     html = requests.get(url).text
-    soup = BeautifulSoup(html, 'html.parser')
-    tbody = soup.find('tbody')
+    soup = BeautifulSoup(html, "html.parser")
+    tbody = soup.find("tbody")
     try:
-        tds = tbody.find_all('td')
+        tds = tbody.find_all("td")
     except AttributeError as e:
-        raise Exception("This is too early to fetch those Holidays, try again in an year!")
+        raise Exception(
+            "This is too early to fetch those Holidays, try again in an year!"
+        )
     holidays = []
     for td in tds:
-        if td.find('div', attrs={'class': 'item'}) is not None:
-            if td.find('div', attrs={'class': 'greenCal'}):
-                name = td.find('div', attrs={'class': 'greenCal'}).contents[0]
-                holiday = {'date': td['data-date'],
-                           'day': td['headers'][0],
-                           'holiday': name,
-                           'holiday_type': 'Restricted'
-                           }
+        if td.find("div", attrs={"class": "item"}) is not None:
+            if td.find("div", attrs={"class": "greenCal"}):
+                name = td.find("div", attrs={"class": "greenCal"}).contents[0]
+                holiday = {
+                    "date": td["data-date"],
+                    "day": td["headers"][0],
+                    "holiday": name,
+                    "holiday_type": "Restricted",
+                }
                 holidays.append(holiday)
-            elif td.find('div', attrs={'class': 'redCal'}):
-                name = td.find('div', attrs={'class': 'redCal'}).contents[0]
-                holiday = {'date': td['data-date'],
-                           'day': td['headers'][0],
-                           'holiday': name,
-                           'holiday_type': 'Gazetted'
-                           }
+            elif td.find("div", attrs={"class": "redCal"}):
+                name = td.find("div", attrs={"class": "redCal"}).contents[0]
+                holiday = {
+                    "date": td["data-date"],
+                    "day": td["headers"][0],
+                    "holiday": name,
+                    "holiday_type": "Gazetted",
+                }
                 holidays.append(holiday)
     return holidays
 
@@ -60,16 +64,16 @@ def generate_dataset(year: str, path: str):
 
 
 def main():
-    '''
+    """
     usage
     ---
     python national_holidays.py -y <year> -p <path>
-    '''
+    """
     year = args().year
     path = args().path
     print(" Fetching list of Holidays for the year: ", year)
     generate_dataset(year=year, path=path)
-    print(f'Holiday Dataset for the year {year} generated!')
+    print(f"Holiday Dataset for the year {year} generated!")
 
 
 if __name__ == "__main__":
